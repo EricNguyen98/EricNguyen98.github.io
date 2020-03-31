db3a2fa733d0ec5fa43d8b97b44c5cfb5556c813
EricNguyen98 <60360013+EricNguyen98@users.noreply.github.com>
2020-03-31 17:35:31 -0400

    Convert Lab 6 to Full HTML

--- asst_2/main.js
+++ asst_2/main.js
@@ -8,26 +8,27 @@ const overlay = document.querySelector('.overlay');
 /*Code from MDN  https://mdn.github.io/learning-area/javascript/building-blocks/gallery/*/
 
 for(let i = 1; i <= 5; i++) {
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', 'images/pic' + i + '.jpg');
-    thumbBar.appendChild(newImage);
-    newImage.onclick = function(e) {
-      displayedImage.src = e.target.src;
-    }
+  const Image = document.createElement('img');
+  Image.setAttribute('src', 'images/pic' + i + '.jpg');
+  thumbBar.appendChild(Image);
+  Image.onclick = function(x) {
+    displayedImage.src = x.target.src;
   }
+}
+
 /* Wiring up the Darken/Lighten button */
 /*Code from MDN  https://mdn.github.io/learning-area/javascript/building-blocks/gallery/*/
 
 btn.onclick = function() {
-    const btnClass = btn.getAttribute('class');
-    if(btnClass === 'dark') {
-      btn.setAttribute('class','light');
-      btn.textContent = 'Lighten';
-      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-    } else {
-      btn.setAttribute('class','dark');
-      btn.textContent = 'Darken';
-      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
-    }
+  const buttonState = btn.getAttribute('class');
+  if(buttonState === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.setAttribute('style','background-color:rgba(0,0,0,0.5)');
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.setAttribute('style','background-color:rgba(0,0,0,0)');
   }
+}
   

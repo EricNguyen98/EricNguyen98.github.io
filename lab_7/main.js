d83e9734c1159cc4f710ed8856c3c30e42b5c52b
EricNguyen98 <60360013+EricNguyen98@users.noreply.github.com>
2020-03-31 23:43:29 -0400

    Lab 7

--- lab_7/main.js
+++ lab_7/main.js
@@ -2,7 +2,7 @@ const mymap = L.map('mapid').setView([38.9869, -76.9426], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 17,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
@@ -12,3 +12,27 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 const marker = L.marker([38.988251, -76.941577]).addTo(mymap);
 marker.bindPopup("Hornbake Library").openPopup();
 
+const foodIns = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+
+let longLat = []
+let name = []
+
+//get coordinates
+fetch(foodIns).then(
+    function(response){
+        return response.json();
+    }
+).then(function(jsonData){
+    for(let i = 0; i <3; i++){
+        longLat[i]=(jsonData[i+3].geocoded_column_1.coordinates.reverse())
+        name[i]=jsonData[i+3].name
+        /*cord = jsonData[i+3].geocoded_column_1.coordinates.reverse()
+        const mark = L.marker(cord).addTo(mymap);
+        mark.bindPopup(jsonData[i+3].name)
+        console.log(cord);*/
+    }
+    for(let j = 0;j<3;j++){
+        const mark = L.marker(longLat[j]).addTo(mymap);
+        mark.bindPopup(name[j])
+    }
+});
